b19b7f693b9abaafe0f336662599ec1fbaa356b3
Łukasz Markowski <lucas20193@gmail.com>
2026-06-07 10:45:48 +0200

    feat: set tournament not started

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -10,8 +10,8 @@ export const appConfig: AppConfig = {
   tournament: {
     id: "world-cup-2026",
     name: "World Cup 2026",
-    // status: "not-started",
-    status: "in-progress",
+    status: "not-started",
+    // status: "in-progress",
   },
   scoring: {
     exactScorePoints: 5,
