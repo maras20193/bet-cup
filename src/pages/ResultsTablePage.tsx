3dd9d57d25267352eabb2fba89c55cd2a12fbffb
Łukasz Markowski <lucas20193@gmail.com>
2026-06-14 10:32:02 +0200

    feat: remove points legend from table page

--- src/pages/ResultsTablePage.tsx
+++ src/pages/ResultsTablePage.tsx
@@ -1,12 +1,11 @@
 import { ResultsTable } from "@/components/results/results-table"
-import { PointsLegend } from "@/components/results/results-table/ui/PointsLegend"
 
 export function ResultsTablePage() {
   return (
     <div className="flex flex-col flex-1 gap-4 min-h-0 overflow-hidden">
-      <div className="shrink-0">
+      {/* <div className="shrink-0">
         <PointsLegend />
-      </div>
+      </div> */}
       <ResultsTable fillHeight />
     </div>
   )
